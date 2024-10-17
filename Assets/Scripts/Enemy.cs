271447188719b55e218d922d38ccd9d7b29721f8
XardoX <45951614+XardoX@users.noreply.github.com>
2024-10-17 14:49:12 +0200

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 3;
+
+    public int damage = 100;
+
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    private bool isAlive = true;
+
+    private string enemyName = "Goliat";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isAlive) //jeśli warunek to prawda
+        {
+            //kod tutaj sie wykona    
+        }
+        else if(health > 0)
+        {
+            Debug.Log("cos");
+        }
+        else
+        {
+            //
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Attack()
+    {
+
+    }
+}
